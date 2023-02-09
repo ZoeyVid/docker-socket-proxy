07e483d7d69a786de5fe9f6c644e8335d287a9fc
Zoey <zoey@z0ey.de>
2023-02-09 08:41:09 +0100

    Update start.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 openssl req -nodes -new -x509 -subj '/CN=*' -sha256 -keyout /etc/privkey.pem -out /etc/fullchain.pem -days 365000 > /dev/null 2>&1
 cat /etc/fullchain.pem /etc/privkey.pem | tee /etc/cert.pem > /dev/null 2>&1
-haproxy -df /etc/haproxy/haproxy.cfg
+haproxy -f /etc/haproxy/haproxy.cfg -d
