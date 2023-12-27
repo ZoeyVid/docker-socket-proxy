5a341acdab869dc0c3142c8923caa9aa0456e949
Zoey <zoey@z0ey.de>
2023-12-28 00:42:30 +0100

    remove -W
    Signed-off-by: Zoey <zoey@z0ey.de>

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 openssl req -nodes -new -x509 -subj '/CN=*' -sha256 -keyout /etc/privkey.pem -out /etc/fullchain.pem -days 365000 > /dev/null 2>&1
 cat /etc/fullchain.pem /etc/privkey.pem | tee /etc/cert.pem > /dev/null 2>&1
 if [ "$NOPOST" = "1" ]; then
-haproxy -f /etc/haproxy/haproxy-no-post.cfg -W -db
+haproxy -f /etc/haproxy/haproxy-no-post.cfg -db
 else
-haproxy -f /etc/haproxy/haproxy.cfg -W -db
+haproxy -f /etc/haproxy/haproxy.cfg -db
 fi
