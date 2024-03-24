5c2d9184336019cf67a7092f36f48900013150e7
Zoey <zoey@z0ey.de>
2024-03-24 16:42:56 +0100

    Update start.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-openssl req -nodes -new -x509 -subj '/CN=*' -sha256 -keyout /etc/privkey.pem -out /etc/fullchain.pem -days 365000 > /dev/null 2>&1
-cat /etc/fullchain.pem /etc/privkey.pem | tee /etc/cert.pem > /dev/null 2>&1
+openssl req -nodes -new -x509 -subj '/CN=*' -sha256 -keyout /tmp/privkey.pem -out /tmp/fullchain.pem -days 365000 > /dev/null 2>&1
+cat /tmp/fullchain.pem /tmp/privkey.pem | tee /tmp/cert.pem > /dev/null 2>&1
 if [ "$NOPOST" = "1" ]; then
 haproxy -f /etc/haproxy/haproxy-no-post.cfg -W -db
 else
