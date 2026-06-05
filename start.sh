875029a9f0f76ab03b53f4c0e2ac4ef038cfc330
Zoey <zoey@z0ey.de>
2026-06-05 21:58:20 +0200

    set self signed cert lifetime back to 365000
    Signed-off-by: Zoey <zoey@z0ey.de>

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ ! -s /tmp/privkey.pem ] || [ ! -s /tmp/fullchain.pem ]; then
-  openssl req -new -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -days 365 -nodes -x509 -subj '/CN=*' -sha512 -keyout /tmp/privkey.pem -out /tmp/fullchain.pem
+  openssl req -new -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -x509 -days 365000 -nodes -subj '/CN=*' -sha512 -keyout /tmp/privkey.pem -out /tmp/fullchain.pem
 fi
 if [ ! -s /tmp/cert.pem ]; then
   cat /tmp/fullchain.pem /tmp/privkey.pem > /tmp/cert.pem
